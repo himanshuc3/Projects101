53e40c73f554e2806aa1c1b7d48110400eb18787
Himanshu <himichhabra14@gmail.com>
2017-07-08 11:37:10 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,112 @@
+var projectname = "";
+
+
+
+$(".one").mouseenter(function(){
+		projectname = "";
+		projectname='<div class="onhover"';
+		projectname += '<h1>';
+		projectname += '<span class="num">01</span> Homepage';
+		projectname += '</h1></div>';
+		$(this).append(projectname);
+});
+
+$(".one").mouseleave(function(){
+		
+		$(".onhover").remove();
+		
+});
+
+$(".two").mouseenter(function(){
+		projectname = "";
+		projectname='<div class="onhover"';
+		projectname += '<h1>';
+		projectname += '<span class="num">02</span> SketchPad';
+		projectname += '</h1></div>';
+		$(this).append(projectname);
+});
+
+$(".two").mouseleave(function(){
+		
+		$(".onhover").remove();
+});
+
+$(".three").mouseenter(function(){
+		projectname = "";
+		projectname='<div class="onhover"';
+		projectname += '<h1>';
+		projectname += '<span class="num">03</span> Hangman';
+		projectname += '</h1></div>';
+		$(this).append(projectname);
+});
+
+
+$(".three").mouseleave(function(){
+		
+		$(".onhover").remove();
+});
+
+
+$(".four").mouseenter(function(){
+		projectname = "";
+		projectname='<div class="onhover"';
+		projectname += '<h1>';
+		projectname += '<span class="num">04</span> Madlibs';
+		projectname += '</h1></div>';
+		$(this).append(projectname);
+});
+
+
+$(".four").mouseleave(function(){
+		
+		$(".onhover").remove();
+});
+
+$(".five").mouseenter(function(){
+		projectname = "";
+		projectname='<div class="onhover"';
+		projectname += '<h1>';
+		projectname += '<span class="num">05</span> Tribute';
+		projectname += '</h1></div>';
+		$(this).append(projectname);
+});
+
+
+$(".five").mouseleave(function(){
+		
+		$(".onhover").remove();
+});
+
+$(".six").mouseenter(function(){
+		projectname = "";
+		projectname='<div class="onhover"';
+		projectname += '<h1>';
+		projectname += '<span class="num">06</span> Animate';
+		projectname += '</h1></div>';
+		$(this).append(projectname);
+});
+
+
+$(".six").mouseleave(function(){
+		
+		$(".onhover").remove();
+});
+
+$(".seven").mouseenter(function(){
+		projectname = "";
+		projectname ='<div class="onhover"';
+		projectname += '<h1>';
+		projectname += '<span class="num">07</span> TO-DO';
+		projectname += '</h1></div>';
+		$(this).append(projectname);
+});
+
+
+$(".seven").mouseleave(function(){
+		
+		$(".onhover").remove();
+});
+
+
+
+
